39e295561b43e4c2fc3e7ffe1bc4184c122ff498
Luis Seeback <lseeb002@ucr.edu>
2015-04-12 16:28:01 -0700

    Started main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <unistd.h>
+#include <string>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <stdio.h>
+#include <errno.h>
+
+using namespace std;
+
+int main()
+{
+	
+	return 0;
+}
