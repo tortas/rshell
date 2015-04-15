ab154500cbbd52c04ab8374d3baa342089d86992
Luis Seeback <lseeb002@ucr.edu>
2015-04-15 15:11:20 -0700

    Updated Makefile and main.cpp

--- Makefile
+++ Makefile
@@ -1,5 +1,4 @@
 all: rshell
-	mkdir bin; mv rshell bin
 
 rshell: 
 	g++ -Wall -Werror -ansi -pedantic src/main.cpp -o rshell
--- src/main.cpp
+++ src/main.cpp
@@ -11,17 +11,30 @@ using namespace std;
 
 int main()
 {
-	char usrIn[128];
-	char *token;
-	cout << "% ";
-	fgets(usrIn, 128, stdin);
-	token = strtok(usrIn, " ");
-
-	while (strcmp(usrIn, "exit\n") != 0)
+	cout << "$ ";
+	char usrString[256];
+	cin.getline(usrString, 256, '\n');
+	char *args[256];
+	char *temp = strtok(usrString, " \n");
+	int pos = 0;
+	int cnt = 0;
+	while (temp != 0)
 	{
-		cout << "% ";
-		fgets(usrIn,128,stdin);
+		args[pos] = temp;
+		++pos;
+		++cnt;
+		temp = strtok(NULL, " ");
+	}
+	args[cnt] = NULL;
+	cout << args[0] << endl;
+	if(-1 == execvp(args[0], args)){
+		perror("execvp");
 	}
 
+	for (int i = 0; i < cnt; ++i)
+	{
+		cout << "token: "<< args[i] << endl;
+	}	
 	return 0;
 }
+
