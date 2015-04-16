2f8fd3cc1f5ea82d07f8f98b8cab1844744bdb05
Luis Seeback <lseeb002@ucr.edu>
2015-04-16 11:30:42 -0700

    created check_connector function

--- src/main.cpp
+++ src/main.cpp
@@ -6,35 +6,73 @@
 #include <sys/wait.h>
 #include <stdio.h>
 #include <errno.h>
+#include <vector>
 
 using namespace std;
 
+int check_connect(const string& str, int& pos, int start){
+	unsigned found = 0;
+	found = str.find_first_of(";&|#",start);
+	cout << "FLAG: entering check function" << endl;
+	if (found >= str.size()){
+		pos = -1;
+		return -1;
+	}
+	else{
+		cout << "FLAG: checking connectors" << endl;
+		if (str.at(found) == ';'){
+			pos = found;
+			return 1;
+		}
+		else if (str.at(found) == '&'){
+			//FIX check for next &
+			cout << "FLAG: found &" << endl;
+			if (str.at(found+1) == '&'){
+				pos = found;
+				return 2;
+			}
+			else{
+				cout << "flag: about to recursively call" << endl;
+				return check_connect(str,pos,found+1);
+			}
+		}
+
+		else if (str.at(found) == '|'){
+			//FIX check for next |
+			if (str.at(found+1) == '|'){
+				pos = found;
+				return 3;
+			}
+			else{
+				cout << "flag: about to recursively call 2" << endl;
+				return check_connect(str,pos,found+1);
+			}
+		}
+		else if (str.at(found) == '#'){
+			pos = found;
+			return 4;
+		}
+	}
+	return -1;
+}
+
 int main()
 {
-	cout << "$ ";
-	char usrString[256];
-	cin.getline(usrString, 256, '\n');
-	char *args[256];
-	char *temp = strtok(usrString, " \n;");
-	int pos = 0;
-	int cnt = 0;
-	while (temp != 0)
-	{
-		args[pos] = temp;
-		++pos;
-		++cnt;
-		temp = strtok(NULL, " ");
+	int start = 0;
+	int c_stat;
+	int pos = -1;
+	vector<string> str;
+	str.push_back("The first string, no connectors");
+	str.push_back("Second string; Semicolon included");
+	str.push_back("&Third: && got that ampersand");
+	str.push_back("We has | in i||t");
+	str.push_back("This has a #comment");
+	cout << "entering for loop" << endl;
+	for (int i = 0; i < 5; ++i){
+		c_stat = check_connect(str.at(i), pos, start);
+		cout << "string " << i << ": " << c_stat << " at: " << pos << endl;		
 	}
-	args[cnt] = NULL;
-	cout << args[0] << endl;
-	if(-1 == execvp(args[0], args)){
-		perror("execvp");
-	}
-
-	for (int i = 0; i < cnt; ++i)
-	{
-		cout << "token: "<< args[i] << endl;
-	}	
 	return 0;
 }
+
 
