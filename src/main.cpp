c9c270f56e4ba39895b80115b13fa99e0ec040c8
Luis Seeback <lseeb002@ucr.edu>
2015-04-16 22:46:02 -0700

    updated parse function and main

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <string.h>
+#include <stdlib.h>
 #include <iostream>
 #include <unistd.h>
 #include <string>
@@ -9,11 +10,32 @@
 #include <vector>
 
 using namespace std;
+	
+void parse_input(char* (&args)[100], string usrString){
+	char *cstr = new char[usrString.length()+1];
+	strcpy(cstr,usrString.c_str());
+	char *temp = strtok(cstr, " &|");
+	int cnt = 0;
 
+	while (temp != 0){
+		args[cnt] = temp;
+		++cnt;
+		temp = strtok(NULL, " &|");
+	}
+	args[cnt] = NULL;
+	for(int i = 0; i < cnt; i++){
+		cout << args[i] << endl;
+	}
+	delete[] cstr;
+	return;
+} 
+
+//Function checks for connectors and returns a number based on which one is found
+//Pass in a string, a position integer, and a starting point integer
 int check_connect(const string& str, int& pos, int start){
 	unsigned found = 0;
 	found = str.find_first_of(";&|#",start);
-	if (found >= str.size()){
+	if (found >= str.size()){	//If not found...
 		pos = -1;
 		return -1;
 	}
@@ -52,19 +74,38 @@ int check_connect(const string& str, int& pos, int start){
 int main()
 {
 	int start = 0;
-	int c_stat;
-	int pos = -1;
+//	int c_stat;
+//	int pos = -1;
 	vector<string> str;
-	str.push_back("The first string, no connectors");
-	str.push_back("Second string; Semicolon included");
+	str.push_back("ls -a");
+	str.push_back("pwd");
 	str.push_back("&Third: && got that ampersand");
 	str.push_back("We has | in i||t");
 	str.push_back("This has a #comment");
-	cout << "entering for loop" << endl;
-	for (int i = 0; i < 5; ++i){
-		c_stat = check_connect(str.at(i), pos, start);
-		cout << "string " << i << ": " << c_stat << " at: " << pos << endl;		
+	char **args = new char*[100];
+	parse_input(*args,str.at(0));
+	cout << "command: " << args[0] << endl;
+	int pid = fork();
+	if (pid == -1){
+		perror("fork");
 	}
+	else if (pid == 0){
+		if(execvp(args[0],args) == -1){
+			perror("execvp");
+		}
+		exit(0);
+	}
+	else if(pid > 0){
+		if(wait(0) == -1){
+			perror("wait");
+		}
+		cout << "Done!" << endl;
+	}
+		
+//	for (int i = 0; i < 5; ++i){
+//		parse_input(args,str.at(i));
+//	}
+	delete[] args;
 	return 0;
 }
 
