25adf779b955a340576b4e34bf6f40352548aa65
Luis Seeback <lseeb002@ucr.edu>
2015-04-19 15:29:37 -0700

    combined parse/exec into one function

--- src/main.cpp
+++ src/main.cpp
@@ -11,10 +11,12 @@
 
 using namespace std;
 	
-void parse_input(char* (&args)[100], string usrString){
-	char *cstr = new char[usrString.length()+1];
+bool parse_exec(string usrString, int size){
+	int status;
+	char **args = new char*[size+1];
+	char *cstr = new char[size+1];
 	strcpy(cstr,usrString.c_str());
-	char *temp = strtok(cstr, " &|");
+	char *temp = strtok(cstr, " &|;#");
 	int cnt = 0;
 
 	while (temp != 0){
@@ -26,8 +28,38 @@ void parse_input(char* (&args)[100], string usrString){
 	for(int i = 0; i < cnt; i++){
 		cout << args[i] << endl;
 	}
+
+	if (!strcmp(args[0],"exit")){
+		exit(0);
+	}
+
+	int pid = fork();
+	if (pid == -1){
+		perror("fork");
+	}
+	else if(pid == 0){
+		if (execvp(args[0],args) == -1){
+			perror("execvp");
+		}
+		exit(0);
+	}
+	else if(pid > 0){
+		if(-1 == wait(&status)){
+			perror("wait");
+		}
+	}
+
 	delete[] cstr;
-	return;
+	delete[] args;
+
+	if(WIFEXITED(status) != 0){
+		cout << "Success!" << endl;
+		return true;
+	}
+	else{
+		cout << "Failure!" << endl;
+		return false;
+	}
 } 
 
 //Function checks for connectors and returns a number based on which one is found
@@ -73,40 +105,106 @@ int check_connect(const string& str, int& pos, int start){
 
 int main()
 {
-	int start = 0;
-//	int c_stat;
-//	int pos = -1;
-	vector<string> str;
-	str.push_back("ls -a");
-	str.push_back("pwd");
-	str.push_back("&Third: && got that ampersand");
-	str.push_back("We has | in i||t");
-	str.push_back("This has a #comment");
-	char **args = new char*[100];
-	parse_input(*args,str.at(0));
-	cout << "command: " << args[0] << endl;
-	int pid = fork();
-	if (pid == -1){
-		perror("fork");
-	}
-	else if (pid == 0){
-		if(execvp(args[0],args) == -1){
-			perror("execvp");
+	//Get User Info
+	
+
+
+	while(1){
+		//Get User Input
+		bool exec_stat = true;		//Connector Status
+		int con_stat = 0;
+		int pos = 0;
+		int start = 0;
+		int size = 0;
+		string usrString;
+		cout << "$ ";
+		getline(cin,usrString); size = usrString.size();
+		if (size == 0){
+			continue;
 		}
-		exit(0);
-	}
-	else if(pid > 0){
-		if(wait(0) == -1){
-			perror("wait");
-		}
-		cout << "Done!" << endl;
-	}
 		
-//	for (int i = 0; i < 5; ++i){
-//		parse_input(args,str.at(i));
-//	}
-	delete[] args;
-	return 0;
+
+		//Check Connectors
+		while(1){
+			con_stat = check_connect(usrString,pos,start);
+			if (con_stat == -1){
+				exec_stat = parse_exec(usrString.substr(start),size);
+				break;
+			}
+			else if (con_stat == 1){
+				exec_stat = parse_exec(usrString.substr(start,pos), size);
+				start = pos + 1;
+				continue;
+			}
+			else if (con_stat == 2){
+				exec_stat = parse_exec(usrString.substr(start,pos), size);
+				start = pos + 2;
+				if (exec_stat == true){
+					continue;
+				}
+				else{
+					break;
+				}
+			}
+			else if (con_stat == 3){
+				exec_stat = parse_exec(usrString.substr(start,pos), size);
+				start = pos + 2;
+				if (exec_stat == true){
+					break;
+				}
+				else{
+					continue;
+				}
+			}
+			else if (con_stat == 4){
+				exec_stat = parse_exec(usrString.substr(start,pos), size);
+				break;
+			}
+		}
+	}
+		return 0;
+
 }
-
-
+	//	while (pos != -1){	
+	//		//Parsing and Executing Section
+	//		char **args = new char*[usrString.length()+1];
+	//		char *cstr = new char[usrString.length()+1];
+	//		strcpy(cstr,usrString.c_str());
+	//		char *temp = strtok(cstr, " &|");
+	//		int cnt = 0;                                
+	//		                                            
+	//		while (temp != 0){                          
+	//			args[cnt] = temp;                       
+	//			++cnt;                                  
+	//			temp = strtok(NULL, " &|");             
+	//		}                                           
+	//		args[cnt] = NULL;                           
+	//		for(int i = 0; i < cnt; i++){               
+	//			cout << args[i] << endl;                
+	//		}                                           
+	//			
+	//		//Execution
+	//		if(!strcmp(args[0], safe_word)){		//Check for exit command
+	//			exit(0);
+	//		}
+	//	
+	//		int pid = fork();
+	//		 if (pid == -1){
+	//			perror("fork");
+	//		}
+	//		else if (pid == 0){
+	//			if(execvp(args[0],args) == -1){
+	//				perror("execvp");
+	//			}
+	//			exit(0);
+	//		}
+	//		else if(pid > 0){
+	//			if(wait(0) == -1){
+	//				perror("wait");
+	//			}
+	//		}
+	//		
+	//
+	//		delete[] cstr;                              
+	//		delete[] args;
+	//		}
